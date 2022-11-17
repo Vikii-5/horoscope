2533753ee87dfe9c050f6ea9f7c3f4ba41234f0b
Vikii-5 <72004386+Vikii-5@users.noreply.github.com>
2022-11-17 11:09:13 +0530

    async function added to fetch and image compressed

--- script.js
+++ script.js
@@ -4,47 +4,39 @@ let zodiacSign = document.getElementById("zodiac-select");
 let selectedDay = document.getElementById("day-select");
 let output = document.getElementById("output-container");
 
-// adding click event to submit button inorder to fetch the targeted data
-submitBtn.addEventListener("click", () => {
+// creating async function to fetch the data
+async function horoscope() {
   let zodiac = zodiacSign.value;
   let day = selectedDay.value;
   let URL = `https://aztro.sameerkumar.website/?sign=${zodiac}&day=${day}`;
-  console.log(URL);
 
-  fetch(URL, {
+  const response = await fetch(URL, {
     method: "POST",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data.current_date);
-      // console.log(data.description);
-      // console.log(data.mood);
-      // console.log(data.color);
-      // console.log(data.lucky_number);
+  });
+  const data = await response.json();
 
-      output.innerHTML = `
-        <div id="output" class="row p-4 mt-5 align-items-center">
-        <div id="image-cont" class="col-md-4 col-sm-12 mb-lg-0 mb-sm-4 text-center">
-            <div id="img-container">
-                <img src="images/${zodiac}.png" alt="${zodiac}">
-            </div>
-            <div class="img-caption mt-3 text-light text-center">
-                <span>${zodiac.toUpperCase()}</span>
-            </div>
-        </div>
-        <div class="col-md-8 col-sm-12">
-            <div class="text-container text-light">
-                <p class="text-start curr-date">${data.current_date}</p>
-                <p class="mb-3">${data.description}</p>
-                <div class="others d-flex flex-md-row flex-sm-column">
-                    <p id="mood" class="me-5">Mood: ${data.mood}</p>
-                    <p id="color" class="me-5">Color: ${data.color}</p>
-                    <p id="lucky-number">Lucky Number: ${data.lucky_number}</p>
-                </div>
-            </div>
-        </div>    
-        </div>`;
-    });
+  output.innerHTML = `
+      <div id="output" class="row p-4 mt-5 align-items-center">
+      <div id="image-cont" class="col-md-4 col-sm-12 mb-lg-0 mb-sm-4 text-center">
+          <div id="img-container">
+              <img src="images/${zodiac}.png" alt="${zodiac}">
+          </div>
+          <div class="img-caption mt-3 text-light text-center">
+              <span>${zodiac.toUpperCase()}</span>
+          </div>
+      </div>
+      <div class="col-md-8 col-sm-12">
+          <div class="text-container text-light">
+              <p class="text-start curr-date">${data.current_date}</p>
+              <p class="mb-3">${data.description}</p>
+              <div class="others d-flex flex-md-row flex-sm-column">
+                  <p id="mood" class="me-5">Mood: ${data.mood}</p>
+                  <p id="color" class="me-5">Color: ${data.color}</p>
+                  <p id="lucky-number">Lucky Number: ${data.lucky_number}</p>
+              </div>
+          </div>
+      </div>    
+      </div>`;
 
   // changing the background image and logo color according to selected zodiac sign
   let body = document.querySelector("body");
@@ -114,4 +106,7 @@ submitBtn.addEventListener("click", () => {
       body.style.backgroundImage = "url(images/bg-01.jpg)";
     }
   }
-});
+}
+
+// adding click event to submit button inorder to fetch the targeted data
+submitBtn.addEventListener("click", horoscope);
